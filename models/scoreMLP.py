5d9ccb8e5ec1e25605ebd50b3a0734c1e324f44c
Florentin Acker <florentinacker@gmail.com>
2026-09-09 12:55:27 +0200

    finished models/scoreMLP

--- models/scoreMLP.py
+++ models/scoreMLP.py
@@ -17,4 +17,31 @@ class ScoreMLP(nn.Module):
         inp = torch.cat([x, sigma], dim=-1)
         return self.net(inp)
 
-        
+def score(x, sigma, model):
+    s_tilde = model(x, sigma)
+    if sigma.dim() == 1:
+        sigma = sigma.unsqueeze(-1)
+    s = s_tilde / sigma
+    return s
+# %%
+def v(x, sigma, model):
+    if sigma.dim() == 1:
+        sigma = sigma.unsqueeze(-1)
+    return sigma**2 * score(x, sigma, model) + x
+
+def v_jvp(x, sigma, model, g):
+    x = x.detach().requires_grad_(True)
+    p = v(x, sigma, model)
+    g = g.detach()
+    y = (p * g).sum()
+    grad = torch.autograd.grad(y, x)[0]
+    return grad
+
+def full_jacobian(x: torch.Tensor, sigma: torch.Tensor, model: ScoreMLP) -> torch.Tensor:
+    d = x.shape[-1]
+    rows = []
+    for i in range(d):
+        g = torch.zeros_like(x)
+        g[:, i] = 1.0
+        rows.append(v_jvp(x, sigma, model, g))
+    return torch.stack(rows, dim=1)
