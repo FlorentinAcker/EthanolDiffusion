7d9560ac21bfa1edc0ef3ea77cbd66affb84ac72
Florentin Acker <florentinacker@gmail.com>
2026-09-09 10:02:24 +0200

    MLP written

--- models/scoreMLP.py
+++ models/scoreMLP.py
@@ -1,5 +1,20 @@
 #%%
-import numpy as np
 import torch
-import scipy as sp
+import torch.nn as nn
 #%%
+class ScoreMLP(nn.Module):
+    def __init__(self, dim:int=4, hidden: int=128, n_layers: int=4):
+        super().__init__()
+        layers = [nn.Linear(dim + 1, hidden), nn.SiLU()]
+        for _ in range(n_layers - 1):
+            layers += [nn.Linear(hidden, hidden), nn.SiLU()]
+        layers += [nn.Linear(hidden, dim)]
+        self.net = nn.Sequential(*layers)
+
+    def forward(self, x:torch.Tensor, sigma: torch.Tensor):
+        if sigma.dim() == 1:
+            sigma = sigma.unsqueeze(-1)
+        inp = torch.cat([x, sigma], dim=-1)
+        return self.net(inp)
+
+        
